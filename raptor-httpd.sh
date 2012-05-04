7fc45fd34be4ba68b0d4704970d5ec2c11aa1354
Andrew Cook <ariscop@gmail.com>
2012-05-04 22:02:35 +1000

    cgi suport, now somewhat working (does not parse response from pgp-cgi, assumes success)

--- raptor-httpd.sh
+++ raptor-httpd.sh
@@ -2,27 +2,10 @@
 
 serve_cgi() {
     export \
-    SERVER_SOFTWARE="Raptor Httpd" \
-    SERVER_NAME="localhost" \
-    SERVER_ADDR="127.0.0.1" \
-    SERVER_PORT="8080" \
-    GATEWAY_INTERFACE="CGI/1.1" \
-    SERVER_PROTOCOL="HTTP/1.0" \
-    REQUEST_METHOD="$1" \
-    SCRIPT_NAME="$2" \
-    QUERY_STRING="$3" \
-    SCRIPT_FILENAME="$4"
-
-    # — the part of URL after ? character. May be composed of *name=value pairs separated with ampersands (such as var1=val1&var2=val2…) when used to submit form data transferred via GET method as defined by HTML application/x-www-form-urlencoded.
-    #REMOTE_HOST — host name of the client, unset if server did not perform such lookup.
-    #REMOTE_ADDR — IP address of the client (dot-decimal).
-    #AUTH_TYPE — identification type, if applicable.
-    #REMOTE_USER used for certain AUTH_TYPEs.
-    #REMOTE_IDENT — see ident, only if server performed such lookup.
-    #CONTENT_TYPE — MIME type of input data if PUT or POST method are used, as provided via HTTP header.
-    #CONTENT_LENGTH — similarly, size of input data (decimal, in octets) if provided via HTTP header.
-    #Variables passed by user agent (HTTP_ACCEPT, HTTP_ACCEPT_LANGUAGE, HTTP_USER_AGENT, HTTP_COOKIE and possibly others) contain values of corresponding HTTP headers and therefore have the same sense.
-	
+    SCRIPT_NAME="$DOCUMENT_URI" \
+    SCRIPT_FILENAME="$1" \
+    REDIRECT_STATUS=200
+    	
 	echo running php
 	echo "HTTP/1.0 200 OK" >&3
 	php-cgi 0<&3 1>&3
@@ -33,20 +16,23 @@ serve_cgi() {
 
 
 serve_file() {
-	MIMETYPE="`file --mime-type -b "$1"`"
-	echo "HTTP/1.0 200 OK" >&3
-	echo "Content-Type: $MIMETYPE" >&3
-	echo >&3
-	echo "$1" 200 "$MIMETYPE"
+	MIMETYPE="`file --mime-type -b \"$1\"`"
+	echo \
+"HTTP/1.0 200 OK
+Content-Type: $MIMETYPE
+
+" >&3
 	cat "$FILENAME" >&3
 }
 
 serve_dir() {
-	echo "HTTP/1.0 200 OK" >&3
-	echo "Content-Type: text/plain" >&3
-	echo >&3
-	echo "$1" 200 text/plain directory listing
-	echo "Directory listing of " "$1" >&3
+	echo \
+"HTTP/1.0 200 OK
+Content-Type: text/plain
+
+$1 200 text/plain directory listing
+Directory listing of $1
+" >&3
 	ls -l "$1" 1>&3 2>&3
 }
 
@@ -61,7 +47,11 @@ do_404() {
 	echo "$FILENAME" not found
 }
 
-
+do_rewrite() {
+	if [ ! -f "$FILENAME" ]; then
+		export FILENAME="$DOCUMENT_ROOT"/index.php
+	fi
+}
 
 http_accept() {
 	read -r REQ <&3
@@ -79,20 +69,24 @@ http_accept() {
 	
 	HEAD=`parse_line "$LINE"`
 	while [ "$HEAD" ]; do
-		export HTTP_$HEAD="`echo "$LINE" | cut -d" " -f2-`"
+		export HTTP_$HEAD="`echo \"$LINE\" | cut -d\  -f2-`"
 		read -r LINE <&3
 		HEAD=`parse_line "$LINE"`
 	done
 	################
 	# ^^ its best this is left alone
 	
-	METHOD="`echo "$REQ" | cut -d' ' -f1`"
-	REQ="`echo "$REQ" | cut -d' ' -f2`"
-	REQPATH="`echo $REQ | cut -d? -f1`"
-	QUERY="`echo $REQ | cut -d? -f2`"
-	FILENAME="$DOCUMENT_ROOT$REQPATH"
-	
-	export FILENAME
+	export	REQUEST_URI="`echo \"$REQ\" | cut -d' ' -f2`"
+	export  DOCUMENT_URI="`echo \"$REQUEST_URI\" | cut -d? -f1`" \
+			QUERY_STRING="`echo \"$REQUEST_URI\" | cut -d? -f2`" \
+			SERVER_SOFTWARE="Raptor Httpd" \
+			SERVER_NAME="localhost" \
+			SERVER_ADDR="127.0.0.1" \
+			SERVER_PORT="8080" \
+			GATEWAY_INTERFACE="CGI/1.1" \
+			SERVER_PROTOCOL="HTTP/1.0" \
+			REQUEST_METHOD="`echo \"$REQ\" | cut -d' ' -f1`"
+	export	FILENAME="$DOCUMENT_ROOT$DOCUMENT_URI"
 	
 	#block ..
 	if [ `echo $FILENAME | grep -F ".."` ]; then
@@ -100,13 +94,16 @@ http_accept() {
 		return
 	fi
 	
-	case "$METHOD" in
+	do_rewrite
+	echo serving "$FILENAME"
+	
+	case "$REQUEST_METHOD" in
 		"GET")
 			if [ -r "$FILENAME" ] ; then
 				if [ -f "$FILENAME" ]; then
-					if [[ "$REQPATH" == "${REQPATH%.php}.php" ]] ; then
+					if [[ "$FILENAME" == "${FILENAME%.php}.php" ]] ; then
 						echo running serve_cgi
-						serve_cgi "$METHOD" "$REQPATH" "$QUERY" "$FILENAME"
+						serve_cgi "$FILENAME"
 					else
 						serve_file "$FILENAME"
 					fi
@@ -136,7 +133,7 @@ if [ "$1" ]; then
 		echo invalid directory
 		exit
 	else
-		export DOCUMENT_ROOT=`echo $1 | sed -E s:/?$:/:`
+		export DOCUMENT_ROOT=`echo $1 | sed -r s:/?$:/:`
 	fi
 else
 	export DOCUMENT_ROOT="`pwd`/"
